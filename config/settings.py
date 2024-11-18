d533ea84b8445b794fc1d531d2f89ab62eaf026c
erfan ashki <erfanbayern111@gmail.com>
2024-11-18 15:52:17 +0330

    add static files & add html of book project

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static config
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
